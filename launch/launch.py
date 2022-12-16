2d468563f2f89e7bff97a09218386e39306e2cb7
stark-2000 <dhineshrajasekaran@gmail.com>
2022-12-16 04:49:19 -0500

    Updated code with ROS bag

--- launch/launch.py
+++ launch/launch.py
@@ -1,14 +1,20 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import ExecuteProcess, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition
 import os
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     record_flag = LaunchConfiguration('record_flag')
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'record_flag',
+            default_value='False'
+        ),
+
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch'),'/turtlebot3_world.launch.py'])
         ),
@@ -17,6 +23,7 @@ def generate_launch_description():
             executable='Walker_Algo',
         ),
         ExecuteProcess(
+        condition=IfCondition(record_flag),
         cmd=[
             'ros2', 'bag', 'record', '-o Walker_Algo_bag', '-a', '-x "/camera.+"'
         ],
